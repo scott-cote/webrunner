11c64b60056309794fc711e0ec90894de26c9a21
Scott Cote <cote.scott.f@gmail.com>
2016-09-07 21:20:05 -0400

    Refactored plugin calls

--- dist/pluginTypes/proxy.js
+++ dist/pluginTypes/proxy.js
@@ -1,8 +1,11 @@
 
 module.exports = function(info) {
 
+  var log = !(info.args.verbose) ? () => {} : (msg) => console.log(msg);
+
   var proxyCallback = function() {
     return new Promise((resolve, reject) => {
+      log('proxyCallback started');
       this.serverResponse.on('end', resolve).on('error', reject);
       this.response.statusCode = this.serverResponse.statusCode;
       Object.keys(this.serverResponse.headers).forEach((key) => {
