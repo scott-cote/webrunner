fe3c67f1044e22cba982cd7db2bde4745a484dab
Scott Cote <cote.scott.f@gmail.com>
2016-09-13 19:06:24 -0400

    A set of bug fixes

--- dist/fullProxy.js
+++ dist/fullProxy.js
@@ -3,52 +3,72 @@ var url = require('url');
 var net = require('net');
 
 var runFullProxy = function() {
-  var options = { port: 5150 };
+ var options = { port: 5150 };
 
-  var handleRequest = function(clientRequest, clientResponse) {
-    try {
-      var originUrl = url.parse(clientRequest.url);
-      var options = {
-        hostname: originUrl.hostname,
-        port: originUrl.port,
-        path: originUrl.path,
-        method: clientRequest.method,
-        headers: clientRequest.headers
-      };
-      var originRequest = http.request(options, (originResponse) => {
-        originResponse.on('error', e => console.log(e));
-        originResponse.pipe(clientResponse);
-      })
-      clientRequest.on('error', e => console.log(e));
-      clientResponse.on('error', e => console.log(e));
-      originRequest.on('error', e => console.log(e));
-      clientRequest.pipe(originRequest);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  var handleConnect = function(clientRequest, clientSocket, data) {
-    try {
-      var originUrl = url.parse(`https://${clientRequest.url}`);
-      var originSocket = net.connect(originUrl.port, originUrl.hostname, () => {
-        clientSocket.write('HTTP/1.1 200 Connection Established\r\n'+'Proxy-agent: WebRunner\r\n'+'\r\n');
-        originSocket.write(data);
-        originSocket.pipe(clientSocket);
-        clientSocket.pipe(originSocket);
+    var buildRequestHeaders = function(headers) {
+      var newHeaders = {};
+      Object.keys(headers).forEach((key) => {
+        if (!['connection','proxy-connection'].find(element => key === element)) {
+          newHeaders[key] = headers[key];
+        }
       });
-      clientRequest.on('error', e => console.log(e));
-      clientSocket.on('error', e => console.log(e));
-      originSocket.on('error', e => console.log(e));
-    } catch(e) {
-      console.log(e);
-    }
-  };
+      return newHeaders;
+    };
 
-  http.createServer(handleRequest).on('connect', handleConnect)
-    .listen(options.port, function() {
-      console.log("WebRunner listening on: http://localhost:"+options.port);
-    }).on('error', e => console.log(e));
+    var logError = function(source, error, allowedErrno) {
+        if (!(allowedErrno||[]).find(errno => error.errno === errno)) {
+            console.log(source+': '+error);
+        }
+    };
+
+ var handleRequest = function(clientRequest, clientResponse) {
+   try {
+     var originUrl = url.parse(clientRequest.url);
+     var options = {
+       hostname: originUrl.hostname,
+       port: originUrl.port,
+       path: originUrl.path,
+       method: clientRequest.method,
+       headers: buildRequestHeaders(clientRequest.headers)
+     };
+     var originRequest = http.request(options, (originResponse) => {
+       originResponse.on('error', e => logError('originResponse', e));
+       clientResponse.statusCode = originResponse.statusCode;
+       Object.keys(originResponse.headers).forEach((key) => {
+         clientResponse.setHeader(key, originResponse.headers[key]);
+       });
+       originResponse.pipe(clientResponse);
+     })
+     clientRequest.on('error', e => logError('clientRequest', e));
+     clientResponse.on('error', e => logError('clientResponse', e));
+     originRequest.on('error', e => logError('originRequest', e));
+     clientRequest.pipe(originRequest);
+   } catch (e) {
+     console.log(e);
+   }
+ };
+
+ var handleConnect = function(clientRequest, clientSocket, data) {
+   try {
+     var originUrl = url.parse(`https://${clientRequest.url}`);
+     var originSocket = net.connect(originUrl.port, originUrl.hostname, () => {
+       clientSocket.write('HTTP/1.1 200 Connection Established\r\n'+'Proxy-agent: WebRunner\r\n'+'\r\n');
+       originSocket.write(data);
+       originSocket.pipe(clientSocket);
+       clientSocket.pipe(originSocket);
+     });
+     clientRequest.on('error', e => logError('clientRequest (connect)', e));
+     clientSocket.on('error', e => logError('clientSocket', e, ['ECONNRESET']));
+     originSocket.on('error', e => logError('originSocket', e));
+   } catch(e) {
+     console.log(e);
+   }
+ };
+
+ http.createServer(handleRequest).on('connect', handleConnect)
+   .listen(options.port, function() {
+     console.log("WebRunner listening on: http://localhost:"+options.port);
+   }).on('error', e => logError('httpServer', e));
 };
 
 module.exports = runFullProxy;
